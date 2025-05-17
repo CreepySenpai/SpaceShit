7262688baf7c71dab9641d7c49f1a9ab3665c45c
CreepySenpai <tuana8255@gmail.com>
2025-05-17 15:04:40 +0700

    Delete stupid base class

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 
 #ifdef CREEPY_INTELLISENSE
-    #include <Creepy/Core/Base.cppm>
     #include <Creepy/Dump.hpp>
 #endif
 
-import creepy.core.base;
 import creepy.game.game;
 
 int main(int argc, char** argv, char** envr){
-
-    Creepy::Base b{};
-    b.doSick();
 
     Creepy::Game game{};
 
