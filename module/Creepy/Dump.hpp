e1702842033e307ff92119405a644eaf797cf4b9
CreepySenpai <tuana8255@gmail.com>
2025-06-04 22:49:03 +0700

    Add some header

--- module/Creepy/Dump.hpp
+++ module/Creepy/Dump.hpp
@@ -2,14 +2,19 @@
 
 #include <cstdint>
 #include <print>
+#include <string_view>
 #include <Creepy/Core/Logger.cppm>
 #include <Creepy/Core/ResourceDescription.cppm>
 #include <Creepy/Core/Type.cppm>
+#include <Creepy/Core/Vector.cppm>
 
 #include <Creepy/Game/Game.cppm>
 
 #include <Creepy/Graphics/GraphicsEngine.cppm>
 #include <Creepy/Graphics/GraphicsDevice.cppm>
 #include <Creepy/Graphics/SwapChain.cppm>
+#include <Creepy/Graphics/DeviceContext.cppm>
+#include <Creepy/Graphics/ShaderBinary.cppm>
+#include <Creepy/Graphics/Buffer.cppm>
 
 #include <Creepy/Window/Window.cppm>
